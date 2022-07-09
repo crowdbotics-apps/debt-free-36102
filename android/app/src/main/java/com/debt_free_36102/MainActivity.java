daef852f99311367c073a34fb68b869f82bfc81d
Crowdbotics <team@crowdbotics.com>
2022-07-09 03:24:14 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/debt_free_36102/MainActivity.java
@@ -0,0 +1,15 @@
+package com.debt_free_36102;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "debt_free_36102";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "debt_free_36102",
+  url: "https://debt_free_36102.botics.co",
+  api: "https://debt_free_36102.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
